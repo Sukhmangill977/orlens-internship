4445d50e446001dc1b9e6f79d16f99b4569c2758
Sukhmangill977 <sukhmangill977@gmail.com>
2024-12-30 16:23:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,23 +13,23 @@ let organizations = [
   {
     id: 1,
     name: "Organization 1",
-    email: "org1@example.com",
-    location: "New York",
+    email: "Paris@example.com",
+    location: "Paris",
     teams: [
       {
         id: 1,
         name: "Team A",
         members: [
-          { id: 1, name: "Member 1", image: "image2.jpg" },
-          { id: 2, name: "Member 2", image: "image2.jpg" }
+          { id: 1, name: "Mem 1", image: "image2.jpg" },
+          { id: 2, name: "Mem 2", image: "image2.jpg" }
         ]
       },
       {
         id: 2,
         name: "Team B",
         members: [
-          { id: 3, name: "Member 3", image: "image2.jpg"},
-          { id: 4, name: "Member 4", image: "image4.jpg" }
+          { id: 3, name: "Mem 3", image: "image2.jpg"},
+          { id: 4, name: "Mem 4", image: "image4.jpg" }
         ]
       }
     ]
